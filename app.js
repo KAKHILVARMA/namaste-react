9423fb8fc66f574c86df5a5cc7f9344993fc82ff
KAKHILVARMA <akhilvarma.kolanuvada@gmail.com>
2025-06-30 22:18:09 +0530

    Second time

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const heading= React.createElement("h1",{},"Hello Shiv from React");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//const heading= React.createElement("h1",{},"Hello Siva");
+//const root=ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+
+const heading=React.createElement("h1",{},"Hi Siva");
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(heading); 
